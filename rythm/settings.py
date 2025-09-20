d2c36d8c2df0dc04c36582aeaf53c8fbd9a92f7a
nahu <63656481+nahugere@users.noreply.github.com>
2025-09-20 12:08:30 +0300

    vercel configs

--- rythm/settings.py
+++ rythm/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['tig-rythm.herokuapp.com', 'localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
